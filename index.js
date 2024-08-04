3966f5f26563de365bfeadeedfd610d835829f06
Rodrigo-0310 <rodrigomariman.03@gmail.com>
2024-08-03 21:54:14 -0400

    Agregando la estructura del proyecto.

---
+++ index.js
@@ -0,0 +1,48 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    // let tituloMensaje = document.getElementById("titulo-mensaje");
+    // let parrafo = document.getElementById("parrafo");
+    // let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/a/gi, "ai")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+
+if (document.getElementById("texto").value.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    document.getElementById("titulo-mensaje").textContent = "Texto encriptado con éxito";
+    document.getElementById("parrafo").textContent = "";
+    document.getElementById("muñeco").src = "./img/encriptado.jpg";
+  } else {
+    document.getElementById("muñeco").src = "./img/muñeco.png";
+    alert("Debes ingresar algún texto");
+   }
+}
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+  
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+    
+      if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/desencriptado.jpg";
+      } else {
+        muñeco.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "warning");
+      }
+  }
