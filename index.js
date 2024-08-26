05b9c8e80fbc67ab05519bcc58a4649cabb80139
Rodrigo-0310 <rodrigomariman.03@gmail.com>
2024-08-25 20:44:10 -0400

    Diseño de la aplicación con css y responsive design junto con la funcionalidad de encriptar un texto

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 function encriptar() {
-    let texto = document.getElementById("texto").value;
-    // let tituloMensaje = document.getElementById("titulo-mensaje");
-    // let parrafo = document.getElementById("parrafo");
-    // let muñeco = document.getElementById("muñeco");
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
 
-    let textoCifrado = texto
-      .replace(/e/gi, "enter")
-      .replace(/i/gi, "imes")
-      .replace(/a/gi, "ai")
-      .replace(/o/gi, "ober")
-      .replace(/u/gi, "ufat");
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
 
-if (document.getElementById("texto").value.length != 0) {
+  if (document.getElementById("texto").value.length != 0) {
     document.getElementById("texto").value = textoCifrado;
     document.getElementById("titulo-mensaje").textContent = "Texto encriptado con éxito";
     document.getElementById("parrafo").textContent = "";
     document.getElementById("muñeco").src = "./img/encriptado.jpg";
   } else {
-    document.getElementById("muñeco").src = "./img/muñeco.png";
+    muñeco.src = "./img/muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Debes ingresar algún texto a encriptar o desencriptar";
     alert("Debes ingresar algún texto");
-   }
+  }
 }
+
 function desencriptar() {
     let texto = document.getElementById("texto").value;
     let tituloMensaje = document.getElementById("titulo-mensaje");
